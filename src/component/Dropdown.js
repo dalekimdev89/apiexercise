e78ab1e75bb6cb104da0ab8a7260d2c6bc62d7d0
dkim <dkim@thedmsgrp.com>
2020-06-18 15:47:23 -0400

    dropdown update

--- src/component/Dropdown.js
+++ src/component/Dropdown.js
@@ -1,23 +1,5 @@
 import React from 'react';
 
-// class Dropdown extends Component {
-
-//    render() {
-//         return (
-//             <div>
-//                 <h2>DROPDOWN</h2>
-//                 <select onChange={ this.props.dropChange }> 
-//                     <option value="https://swapi.dev/api/people/?page=1">Page1</option>
-//                     <option value="https://swapi.dev/api/people/?page=2">Page2</option>
-//                     <option value="https://swapi.dev/api/people/?page=3">Page3</option>
-//                     <option value="https://swapi.dev/api/people/?page=4">Page4</option>
-//                 </select>
-//             </div>
-//         );
-//     }
-
-// }
-
 const Dropdown = ({dropChange}) => {
     return (
         <div>
